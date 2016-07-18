2144df8d701356ef102cdda229acfe42285993c1
BeOker <qq670@DESKTOP-BMG1RST>
2016-07-19 03:30:04 +0800

    changes

--- js/UIRouter.js
+++ js/UIRouter.js
@@ -5,17 +5,23 @@ var myApp = angular.module("myApp", ['ui.router']);
 	     $urlRouterProvider.when("","/");
 	     
 	     $stateProvider
-	        .state("top1", {
-	            url: "/top1",
-	            templateUrl: "../top1.html"
+	        .state("modify_pwd", {
+	            url: "/#modify_pwd",
+	            templateUrl: "../Public_Page/modify_pwd3.html"
 	        })
-	        .state("top2", {
-	            url:"/top2",
-	            templateUrl: "top2.html"
+	        .state("modify_per_info", {
+	            url:"/modify_per_info",
+	            templateUrl: "../Public_Page/modify_per_info.html"
 	        })
-	        .state("top3", {
+	        .state("stu_info", {
+	            url:"/stu_info",
+	            templateUrl: "stu_info.html"
+
+	        });
+	         .state("top3", {
 	            url:"/top3",
 	            templateUrl: "top3.html"
+
 
 	        });
 
