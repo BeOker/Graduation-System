a0305f3806cb81afe953df0e04f4401297afe104
qq670 <qq670@DESKTOP-BMG1RST>
2016-07-21 20:54:02 +0800

    ok

--- js/UIRouter.js
+++ js/UIRouter.js
@@ -29,7 +29,7 @@ myApp.config(function($stateProvider, $urlRouterProvider) {
 			url: "/check_task",
 			templateUrl: "check_task.html"
 		})
-				.state("review_mid_check", {
+		.state("review_mid_check", {
 			url: "/review_mid_check",
 			templateUrl: "review_mid_check.html"
 		})
@@ -57,5 +57,72 @@ myApp.config(function($stateProvider, $urlRouterProvider) {
 			url: "/modify_date",
 			templateUrl: "modify_date.html"
 		})
-
+		.state("review_stu_req", {
+			url: "/review_stu_req",
+			templateUrl: "review_stu_req.html"
+		})
+		.state("review_stu_task", {
+			url: "/review_stu_task",
+			templateUrl: "review_stu_task.html"
+		})
+		.state("review_mid_paper", {
+			url: "/review_mid_paper",
+			templateUrl: "review_mid_paper.html"
+		})
+		.state("review_opening_report", {
+			url: "/review_opening_report",
+			templateUrl: "review_opening_report.html"
+		})
+		.state("review_paper_draft", {
+			url: "/review_paper_draft",
+			templateUrl: "review_paper_draft.html"
+		})
+		.state("review_paper_final", {
+			url: "/review_paper_final",
+			templateUrl: "review_paper_final.html"
+		})
+		.state("check_but_group", {
+			url: "/check_but_group",
+			templateUrl: "check_but_group.html"
+		})
+		.state("teacher_stu_num", {
+			url: "/teacher_stu_num",
+			templateUrl: "teacher_stu_num.html"
+		})
+		.state("check_double_final", {
+			url: "/check_double_final",
+			templateUrl: "check_double_final.html"
+		})
+		.state("arr_read_teacher", {
+			url: "/arr_read_teacher",
+			templateUrl: "arr_read_teacher.html"
+		})
+		.state("arr_but_group", {
+			url: "/arr_but_group",
+			templateUrl: "arr_but_group.html"
+		})
+		.state("grad_stu_info", {
+			url: "/grad_stu_info",
+			templateUrl: "grad_stu_info.html"
+		})
+		.state("stu_topic_info", {
+			url: "/stu_topic_info",
+			templateUrl: "stu_topic_info.html"
+		})
+		.state("stu_task_info", {
+			url: "/stu_task_info",
+			templateUrl: "stu_task_info.html"
+		})
+		.state("opening_report", {
+			url: "/opening_report",
+			templateUrl: "opening_report.html"
+		})
+		.state("stu_score_pool", {
+			url: "/stu_score_pool",
+			templateUrl: "stu_score_pool.html"
+		})
+		.state("arr_but_secretary", {
+			url: "/arr_but_secretary",
+			templateUrl: "arr_but_secretary.html"
+		})
 });
